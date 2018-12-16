640b512fcc41a8eb52af413ff46d406b90191f3e
yehuda.livnoni <yehuda.livnoni@capriza.com>
2018-12-16 22:40:05 +0200

    initial commit

---
+++ arduino.js
@@ -0,0 +1,74 @@
+var five,board;
+
+const SPEED_HIGH = 255;
+const SPEED_MEDIUM = 125;
+const SPEED_LOW = 50;
+
+function arduino(){
+    return new Promise((resolve)=>{
+         five = require("johnny-five");
+         board = new five.Board();
+
+
+        board.on("ready", ()=>{
+            console.log("Board is ready!");
+            var car = new Car();
+            resolve(car);
+        })
+    })
+}
+
+module.exports = arduino;
+
+
+class Car{
+    constructor(){
+        const configs = five.Motor.SHIELD_CONFIGS.ADAFRUIT_V2;
+
+        this.motor1 = new five.Motor(configs.M1);
+        this.motor2 = new five.Motor(configs.M2);
+        this.motor3 = new five.Motor(configs.M3);
+        this.motor4 = new five.Motor(configs.M4);
+    }
+
+    forward(){
+        this.motor1.forward(SPEED_HIGH);
+        this.motor2.forward(SPEED_HIGH);
+        this.motor3.forward(SPEED_HIGH);
+        this.motor4.forward(SPEED_HIGH);
+    }
+
+    backward(){
+
+        this.motor1.reverse(SPEED_HIGH);
+        this.motor2.reverse(SPEED_HIGH);
+        this.motor3.reverse(SPEED_HIGH);
+        this.motor4.reverse(SPEED_HIGH);
+    }
+
+    right(){
+
+        this.motor1.forward(SPEED_HIGH);
+        this.motor2.forward(SPEED_HIGH);
+        this.motor3.reverse(SPEED_HIGH);
+        this.motor4.reverse(SPEED_HIGH);
+    }
+
+
+    left(){
+
+        this.motor1.reverse(SPEED_HIGH);
+        this.motor2.reverse(SPEED_HIGH);
+        this.motor3.forward(SPEED_HIGH);
+        this.motor4.forward(SPEED_HIGH);
+    }
+
+
+    stop(){
+        this.motor1.brake();
+        this.motor2.brake();
+        this.motor3.brake();
+        this.motor4.brake();
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,78 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var car;
+
+// async function getCar(){
+//     car = await require('./arduino')();
+//
+// }
+//
+// getCar();
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', async function(socket){
+    console.log('a user connected');
+
+    if(!car) car = await require('./arduino')();
+
+    var state = "";
+
+    socket.on('robot', function(msg){
+        console.log('robot: ' + msg);
+        switch(msg) {
+            case 'FORWARD':
+                //Arrow up
+                if(state != "FORWARD"){
+                    console.log("FORWARD");
+                    state = "FORWARD";
+                    car.forward();
+                }
+                break;
+            case 'BACKWARD':
+                //Arrow down
+                if(state != "BACKWARD"){
+                    console.log("down");
+                    state = "BACKWARD";
+                    car.backward();
+                }
+                break;
+            case 'RIGHT':
+                //Arrow right
+                if(state != "RIGHT"){
+                    console.log("right");
+                    state = "RIGHT";
+                    car.right();
+                }
+                break;
+            case 'LEFT':
+                //Arrow left
+                if(state != "LEFT"){
+                    console.log("left");
+                    state = "LEFT";
+                    car.left();
+                }
+                break;
+            case 'STOP':
+                //Arrow left
+                if(state != "STOP"){
+                    car.stop();
+                    state = "STOP";
+                    console.log("stop");
+                }
+                break;
+        }
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
