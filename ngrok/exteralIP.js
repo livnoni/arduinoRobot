4b63add79f49091bc5b290f150fc484de6f03930
livnoni <yehuda.livnobi@gmail.com>
2024-03-27 23:13:47 +0200

    default ngrock params

--- ngrok/exteralIP.js
+++ ngrok/exteralIP.js
@@ -16,12 +16,12 @@ const createExternalIPs = async ()=>{
         const url_port_8000 = await ngrok.connect({
             proto: 'http',
             authtoken: config.ngrokToken,
-            addr: 8000,
+            addr: 8081,
             region: config.region,
-            onStatusChange: status => {console.log("url_port_8000 status",status)}, // 'closed' - connection is lost, 'connected' - reconnected
-            onLogEvent: data => {if(config.debugger) console.log("data (url_port_8000): ",data)}
+            onStatusChange: status => {console.log("url_port_8081 status",status)}, // 'closed' - connection is lost, 'connected' - reconnected
+            onLogEvent: data => {if(config.debugger) console.log("data (url_port_8081): ",data)}
         });
-        console.log("url_port_8000: ",url_port_8000);
+        console.log("url_port_8081: ",url_port_8000);
         return [url_port_3000, url_port_8000];
     }catch (e) {
         console.warn('got error while try to create ngrok url - you can still use the robot on your local network!');
