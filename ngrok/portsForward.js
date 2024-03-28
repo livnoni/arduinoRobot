9bd23f798b77ea42c75c0f17f41769c92ddb7142
livnoni <yehuda.livnobi@gmail.com>
2024-03-29 00:03:24 +0200

    improve logs

--- ngrok/portsForward.js
+++ ngrok/portsForward.js
@@ -14,18 +14,18 @@ const portsForward = async ()=>{
         });
 
         // Output ngrok url to console
-        console.log(`Ingress established at: ${serverListener.url()}`);
+        console.log(`Ingress established at: ${serverListener.url()} for port: ${3000}`);
 
 
         // Establish connectivity
         const videoListener = await ngrok.forward({
             proto: 'http',
-            addr: 3000,
+            addr: 8081,
             authtoken: config.ngrokToken
         });
 
         // Output ngrok url to console
-        console.log(`Ingress established at: ${videoListener.url()}`);
+        console.log(`Ingress established at: ${videoListener.url()} for port: ${8081}`);
 
         return true;
     }catch (e) {
